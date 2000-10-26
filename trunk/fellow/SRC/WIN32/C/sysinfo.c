d8ba2069c44d25d7cd155d0733dadca374d05eaa
carfesh <carfesh@5ef2197a-e883-47a9-8f70-2fa63c8abf5e>
2000-10-26 04:35:54 +0000

    small code cleanup

--- trunk/fellow/SRC/WIN32/C/sysinfo.c
+++ trunk/fellow/SRC/WIN32/C/sysinfo.c
@@ -16,6 +16,8 @@
 /* ----------                                                                 */
 /* 2000/10/24: included draft cpu detection code on assembly level; basically */
 /*             this is a port of the MemTest86 routines to VisualC            */
+/* 2000/10/23: additional hardware enumeration tree ROOT for some NT versions */
+/*             has been added                                                 */
 /* 2000/10/21: reads more stuff from the registry, even more experimental     */
 /*             regarding to where to find stuff in different windows versions */
 /*             and how to enumerate keys in two levels...                     */
@@ -29,18 +31,20 @@
 
 /* Older compilers do not support CPUID and RDTSC properly */
 #define cpuid _asm _emit 0x0f _asm _emit 0xa2
-#define rdtsc _asm _emit 0Fh  _asm _emit 031h 
+#define rdtsc _asm _emit 0x0f _asm _emit 0x31
 
-struct cpu_ident {
-	char type;
-	char model;
-	char step;
-	char fill;
-	long thecpuid;
-	long capability;
-	char vend_id[12];
-	unsigned char cache_info[16];
-} cpu_id;
+struct cpu_ident
+{
+  char type;
+  char model;
+  char step;
+  char fill;
+  long thecpuid;
+  long capability;
+  char vend_id[12];
+  unsigned char cache_info[16];
+}
+cpu_id;
 
 #define MYREGBUFFERSIZE 1024
 
@@ -418,13 +422,15 @@ fellowVersionInfo (void)
 /*                                                                            */
 /* lines marked with @@@@@ differ from the original source                    */
 /*============================================================================*/
-static void cpu_get_features(void)
+static void
+cpu_get_features (void)
 {
   /* initialization */
-  memset(&cpu_id, 0, sizeof(cpu_id));
-  
+  memset (&cpu_id, 0, sizeof (cpu_id));
+
   _asm
   {
+    /* *INDENT-OFF* */
 	/* Find out the CPU type */
 
 	mov	cpu_id.thecpuid,	-1			;  -1 for no CPUID initially
@@ -540,421 +546,459 @@ not_amd:
 	mov		dword ptr[cpu_id.cache_info],edx
 
 id_done:
+	/* *INDENT-ON* */
   }
 }
 
-static float cpu_speed(void)
+static float
+cpu_speed (void)
 {
-  unsigned int  Cycle_Start, Cycle_End, ticks, cycles;  
-  unsigned int  result0 = 0, result1 = 0, result2 = 0, SumResults;          
-  long          tries=0;        
-  LARGE_INTEGER Timer_Start, Timer_End, Counter_Frequency;  
-  unsigned int  Priority_Process;
-  long          Priority_Thread;
+  unsigned int Cycle_Start, Cycle_End, ticks, cycles;
+  unsigned int result0 = 0, result1 = 0, result2 = 0, SumResults;
+  long tries = 0;
+  LARGE_INTEGER Timer_Start, Timer_End, Counter_Frequency;
+  unsigned int Priority_Process;
+  long Priority_Thread;
 
   /* get a precise timer if available */
-  if (!QueryPerformanceFrequency(&Counter_Frequency)) return 0;
+  if (!QueryPerformanceFrequency (&Counter_Frequency))
+    return 0;
 
   /* store thread and process information */
-  Priority_Process = GetPriorityClass(GetCurrentProcess());
-  Priority_Thread  = GetThreadPriority(GetCurrentThread());
+  Priority_Process = GetPriorityClass (GetCurrentProcess ());
+  Priority_Thread = GetThreadPriority (GetCurrentThread ());
 
   /* give this thread the highest possible priority to get usable results */
-  SetPriorityClass(GetCurrentProcess(), REALTIME_PRIORITY_CLASS);
-  SetThreadPriority(GetCurrentThread(), THREAD_PRIORITY_TIME_CRITICAL);
+  SetPriorityClass (GetCurrentProcess (), REALTIME_PRIORITY_CLASS);
+  SetThreadPriority (GetCurrentThread (), THREAD_PRIORITY_TIME_CRITICAL);
 
   /* compare elapsed time on timer with elapsed cycles on the TSR */
-  do 
-  {
-	/* run until the results are exact +/- 1 MHz or up to 10 times */
-	tries++;
-	result2 = result1; /* shift results */
-	result1 = result0;
-
-    /* store starting counter */
-    QueryPerformanceCounter(&Timer_Start); 
-		
-    Timer_End.LowPart = Timer_Start.LowPart;		/* initial time */
-	Timer_End.HighPart = Timer_Start.HighPart;
-
-	/* loop until 50 ticks have passed */
-    while ((unsigned int)Timer_End.LowPart - (unsigned int)Timer_Start.LowPart < 50) 
-	  QueryPerformanceCounter(&Timer_End);
-		
-   	_asm
+  do
     {
+      /* run until the results are exact +/- 1 MHz or up to 10 times */
+      tries++;
+      result2 = result1;	/* shift results */
+      result1 = result0;
+
+      /* store starting counter */
+      QueryPerformanceCounter (&Timer_Start);
+
+      Timer_End.LowPart = Timer_Start.LowPart;	/* initial time */
+      Timer_End.HighPart = Timer_Start.HighPart;
+
+      /* loop until 50 ticks have passed */
+      while ((unsigned int) Timer_End.LowPart -
+	     (unsigned int) Timer_Start.LowPart < 50)
+	QueryPerformanceCounter (&Timer_End);
+
+      _asm
+      {
+		/* *INDENT-OFF* */
 		rdtsc				
 		mov		Cycle_Start,	eax
-   	}
+		/* *INDENT-ON* */
+      }
 
-	Timer_Start.LowPart = Timer_End.LowPart;		/* reset initial time */
-	Timer_Start.HighPart = Timer_End.HighPart;
+      Timer_Start.LowPart = Timer_End.LowPart;	/* reset initial time */
+      Timer_Start.HighPart = Timer_End.HighPart;
 
-    /* loop for 1000 ticks */
-    while ((unsigned int)Timer_End.LowPart - (unsigned int)Timer_Start.LowPart < 1000 ) 	
-      QueryPerformanceCounter(&Timer_End);
-			
-	_asm
-	{
+      /* loop for 1000 ticks */
+      while ((unsigned int) Timer_End.LowPart -
+	     (unsigned int) Timer_Start.LowPart < 1000)
+	QueryPerformanceCounter (&Timer_End);
+
+      _asm
+      {
+		/* *INDENT-OFF* */
 		rdtsc
         mov		Cycle_End, eax
+		/* *INDENT-ON* */
+      }
+
+      cycles = Cycle_End - Cycle_Start;
+      ticks =
+	(unsigned int) Timer_End.LowPart - (unsigned int) Timer_Start.LowPart;
+
+      ticks = ticks * 100000;
+      ticks = ticks / (Counter_Frequency.LowPart / 10);
+
+      if (ticks % Counter_Frequency.LowPart > Counter_Frequency.LowPart / 2)
+	ticks++;
+
+      result0 = cycles / ticks;	/* cycles / ticks  = MHz */
+
+      if (cycles % ticks > ticks / 2)
+	result0++;
+
+      SumResults = result0 + result1 + result2;
     }
+  while ((tries < 10) && ((abs (3 * result0 - SumResults) > 3) ||
+			  (abs (3 * result1 - SumResults) > 3)
+			  || (abs (3 * result2 - SumResults) > 3)));
 
-    cycles = Cycle_End - Cycle_Start;
-    ticks = (unsigned int)Timer_End.LowPart - (unsigned int)Timer_Start.LowPart;	
-
-	ticks = ticks * 100000;				
-	ticks = ticks / ( Counter_Frequency.LowPart/10 );		
-		
-	if (ticks%Counter_Frequency.LowPart > Counter_Frequency.LowPart/2)		
-	  ticks++;
-		
-	result0 = cycles/ticks;	/* cycles / ticks  = MHz */
-        									
-    if (cycles%ticks > ticks/2)
-      result0++;
-          	
-	SumResults = result0 + result1 + result2;	
-  } while((tries < 10) && ((abs(3*result0-SumResults) > 3) ||
-		  (abs(3*result1-SumResults) > 3) || (abs(3*result2-SumResults) > 3)));
-		
-  if (SumResults/3 != (SumResults+1)/3)
-    SumResults ++;
+  if (SumResults / 3 != (SumResults + 1) / 3)
+    SumResults++;
 
   // restore the thread priority
-  SetPriorityClass(GetCurrentProcess(), Priority_Process);
-  SetThreadPriority(GetCurrentThread(), Priority_Thread);
+  SetPriorityClass (GetCurrentProcess (), Priority_Process);
+  SetThreadPriority (GetCurrentThread (), Priority_Thread);
 
-  return(((float)SumResults) / 3);
+  return (((float) SumResults) / 3);
 }
 
 /*============================================*/
 /* parse the contents of the cpu_id structure */
 /*============================================*/
-static void DetectCPU (void)
+static void
+DetectCPU (void)
 {
-  int i, off=0;
+  int i, off = 0;
 
-  int l1_cache=0, l2_cache=0;
+  int l1_cache = 0, l2_cache = 0;
   float speed;
   char vendor[13];
 
-  cpu_get_features();
- 
-  fellowAddLog("Processor detection attempt: ");
-  strncpy(vendor, cpu_id.vend_id, 12);
-  fellowAddLog("%s ", vendor);
+  cpu_get_features ();
+
+  fellowAddLog ("Processor detection attempt: ");
+  strncpy (vendor, cpu_id.vend_id, 12);
+  fellowAddLog ("%s ", vendor);
 
   /* If the CPUID instruction is not supported then this is */
   /* a 386, 486 or one of the early Cyrix CPU's */
-  if (cpu_id.thecpuid < 1) {
-    switch (cpu_id.type) {
-	  case 2:
-        /* This is a Cyrix CPU without CPUID */
+  if (cpu_id.thecpuid < 1)
+    {
+      switch (cpu_id.type)
+	{
+	case 2:
+	  /* This is a Cyrix CPU without CPUID */
 
-		  /*
-			i = getCx86(0xfe);
-			i &= 0xf0;
-			i >>= 4;
-			switch(i) {
-			case 0:
-			case 1:
-				fellowAddLog("Cyrix Cx486");
-				break;
-			case 2:
-				fellowAddLog("Cyrix 5x86");
-				break;
-			case 3:
-				fellowAddLog("Cyrix 6x86");
-				break;
-			case 4:
-				fellowAddLog("Cyrix MediaGX");
-				break;
-			case 5:
-				fellowAddLog("Cyrix 6x86MX");
-				break;
-			case 6:
-				fellowAddLog("Cyrix MII");
-				break;
-			default:
-				fellowAddLog("Cyrix ???");
-				break;
-			}
-			*/
-			fellowAddLog("Cytrix - buhbuh...");
-			break;
+	  /*
+	     i = getCx86(0xfe);
+	     i &= 0xf0;
+	     i >>= 4;
+	     switch(i) {
+	     case 0:
+	     case 1:
+	     fellowAddLog("Cyrix Cx486");
+	     break;
+	     case 2:
+	     fellowAddLog("Cyrix 5x86");
+	     break;
+	     case 3:
+	     fellowAddLog("Cyrix 6x86");
+	     break;
+	     case 4:
+	     fellowAddLog("Cyrix MediaGX");
+	     break;
+	     case 5:
+	     fellowAddLog("Cyrix 6x86MX");
+	     break;
+	     case 6:
+	     fellowAddLog("Cyrix MII");
+	     break;
+	     default:
+	     fellowAddLog("Cyrix ???");
+	     break;
+	     }
+	   */
+	  fellowAddLog ("Cyrix - buhbuh...");
+	  break;
 
 	case 3:
-		fellowAddLog("386");
-		break;
+	  fellowAddLog ("386");
+	  break;
 
 	case 4:
-		fellowAddLog("486");
-		l1_cache = 8;
-		break;
-		}
-		return;
+	  fellowAddLog ("486");
+	  l1_cache = 8;
+	  break;
+	}
+      return;
+    }
+
+  switch (cpu_id.vend_id[0])
+    {
+      /* AMD Processors */
+    case 'A':
+      switch (cpu_id.type)
+	{
+	case 4:
+	  switch (cpu_id.model)
+	    {
+	    case 3:
+	      fellowAddLog ("AMD 486DX2");
+	      break;
+	    case 7:
+	      fellowAddLog ("AMD 486DX2-WB");
+	      break;
+	    case 8:
+	      fellowAddLog ("AMD 486DX4");
+	      break;
+	    case 9:
+	      fellowAddLog ("AMD 486DX4-WB");
+	      break;
+	    case 14:
+	      fellowAddLog ("AMD 5x86-WT");
+	      break;
+	    }
+	  /* Since we can't get CPU speed or cache info return */
+	  return;
+	case 5:
+	  switch (cpu_id.model)
+	    {
+	    case 0:
+	    case 1:
+	    case 2:
+	    case 3:
+	      fellowAddLog ("AMD K5");
+	      off = 6;
+	      break;
+	    case 6:
+	    case 7:
+	      fellowAddLog ("AMD K6");
+	      off = 6;
+	      l1_cache = cpu_id.cache_info[3];
+	      l1_cache += cpu_id.cache_info[7];
+	      break;
+	    case 8:
+	      fellowAddLog ("AMD K6-2");
+	      off = 8;
+	      l1_cache = cpu_id.cache_info[3];
+	      l1_cache += cpu_id.cache_info[7];
+	      break;
+	    case 9:
+	      fellowAddLog ("AMD K6-III");
+	      off = 10;
+	      l1_cache = cpu_id.cache_info[3];
+	      l1_cache += cpu_id.cache_info[7];
+	      l2_cache = (cpu_id.cache_info[11] << 8);
+	      l2_cache += cpu_id.cache_info[10];
+	      break;
+	    }
+	  break;
+	case 6:
+	  switch (cpu_id.model)
+	    {
+	    case 1:
+	    case 2:
+	    case 4:
+	      fellowAddLog ("AMD Athlon");
+	      off = 10;
+	      break;
+	    case 3:
+	      fellowAddLog ("AMD Duron");
+	      off = 9;
+	      break;
+	    }
+	  l1_cache = cpu_id.cache_info[3];
+	  l1_cache += cpu_id.cache_info[7];
+	  l2_cache = (cpu_id.cache_info[11] << 8);
+	  l2_cache += cpu_id.cache_info[10];
+	}
+      break;
+
+      /* Intel Processors */
+    case 'G':
+      if (cpu_id.type == 4)
+	{
+	  switch (cpu_id.model)
+	    {
+	    case 0:
+	    case 1:
+	      fellowAddLog ("Intel 486DX");
+	      off = 11;
+	      break;
+	    case 2:
+	      fellowAddLog ("Intel 486SX");
+	      off = 11;
+	      break;
+	    case 3:
+	      fellowAddLog ("Intel 486DX2");
+	      off = 12;
+	      break;
+	    case 4:
+	      fellowAddLog ("Intel 486SL");
+	      off = 11;
+	      break;
+	    case 5:
+	      fellowAddLog ("Intel 486SX2");
+	      off = 12;
+	      break;
+	    case 7:
+	      fellowAddLog ("Intel 486DX2-WB");
+	      off = 15;
+	      break;
+	    case 8:
+	      fellowAddLog ("Intel 486DX4");
+	      off = 12;
+	      break;
+	    case 9:
+	      fellowAddLog ("Intel 486DX4-WB");
+	      off = 15;
+	      break;
+	    }
+	  /* Since we can't get CPU speed or cache info return */
+	  return;
 	}
 
-	switch(cpu_id.vend_id[0]) {
-	/* AMD Processors */
-	case 'A':
-		switch(cpu_id.type) {
-		case 4:
-			switch(cpu_id.model) {
-			case 3:
-				fellowAddLog("AMD 486DX2");
-				break;
-			case 7:
-				fellowAddLog("AMD 486DX2-WB");
-				break;
-			case 8:
-				fellowAddLog("AMD 486DX4");
-				break;
-			case 9:
-				fellowAddLog("AMD 486DX4-WB");
-				break;
-			case 14:
-				fellowAddLog("AMD 5x86-WT");
-				break;
-			}
-			/* Since we can't get CPU speed or cache info return */
-			return;
-		case 5:
-			switch(cpu_id.model) {
-			case 0:
-			case 1:
-			case 2:
-			case 3:
-				fellowAddLog("AMD K5");
-				off = 6;
-				break;
-			case 6:
-			case 7:
-				fellowAddLog("AMD K6");
-				off = 6;
-				l1_cache = cpu_id.cache_info[3];
-				l1_cache += cpu_id.cache_info[7];
-				break;
-			case 8:
-				fellowAddLog("AMD K6-2");
-				off = 8;
-				l1_cache = cpu_id.cache_info[3];
-				l1_cache += cpu_id.cache_info[7];
-				break;
-			case 9:
-				fellowAddLog("AMD K6-III");
-				off = 10;
-				l1_cache = cpu_id.cache_info[3];
-				l1_cache += cpu_id.cache_info[7];
-				l2_cache = (cpu_id.cache_info[11] << 8);
-				l2_cache += cpu_id.cache_info[10];
-				break;
-			}
-			break;
-		case 6:
-			switch(cpu_id.model) {
-			case 1:
-			case 2:
-			case 4:
-				fellowAddLog("AMD Athlon");
-				off = 10;
-				break;
-			case 3:
-				fellowAddLog("AMD Duron");
-				off = 9;
-				break;
-			}
-			l1_cache = cpu_id.cache_info[3];
-			l1_cache += cpu_id.cache_info[7];
-			l2_cache = (cpu_id.cache_info[11] << 8);
-			l2_cache += cpu_id.cache_info[10];
-		}
-		break;
-
-	/* Intel Processors */
-	case 'G':
-		if (cpu_id.type == 4) {
-			switch(cpu_id.model) {
-			case 0:
-			case 1:
-				fellowAddLog("Intel 486DX");
-				off = 11;
-				break;
-			case 2:
-				fellowAddLog("Intel 486SX");
-				off = 11;
-				break;
-			case 3:
-				fellowAddLog("Intel 486DX2");
-				off = 12;
-				break;
-			case 4:
-				fellowAddLog("Intel 486SL");
-				off = 11;
-				break;
-			case 5:
-				fellowAddLog("Intel 486SX2");
-				off = 12;
-				break;
-			case 7:
-				fellowAddLog("Intel 486DX2-WB");
-				off = 15;
-				break;
-			case 8:
-				fellowAddLog("Intel 486DX4");
-				off = 12;
-				break;
-			case 9:
-				fellowAddLog("Intel 486DX4-WB");
-				off = 15;
-				break;
-			}
-			/* Since we can't get CPU speed or cache info return */
-			return;
-		}
-
-		/* Get the cache info */
-		for (i=0; i<16; i++) {
-			switch(cpu_id.cache_info[i]) {
-			case 0x6:
-				l1_cache += 8;
-				break;
-			case 0x8:
-				l1_cache += 16;
-				break;
-			case 0xa:
-				l1_cache += 8;
-				break;
-			case 0xc:
-				l1_cache += 16;
-				break;
-			case 0x40:
-				l2_cache = 0;
-				break;
-			case 0x41:
-				l2_cache = 128;
-				break;
-			case 0x42:
-			case 0x82:
-				l2_cache = 256;
-				break;
-			case 0x43:
-				l2_cache = 512;
-				break;
-			case 0x44:
-			case 0x84:
-				l2_cache = 1024;
-				break;
-			case 0x45:
-			case 0x85:
-				l2_cache = 2048;
-				break;
-			}
-		}
-
-		switch(cpu_id.type) {
-		case 5:
-			switch(cpu_id.model) {
-			case 0:
-			case 1:
-			case 2:
-			case 3:
-			case 7:
-				fellowAddLog("Pentium");
-				if (l1_cache == 0) {
-					l1_cache = 8;
-				}
-				off = 7;
-				break;
-			case 4:
-			case 8:
-				fellowAddLog("Pentium-MMX");
-				if (l1_cache == 0) {
-					l1_cache = 16;
-				}
-				off = 11;
-				break;
-			}
-			break;
-		case 6:
-			switch(cpu_id.model) {
-			case 0:
-			case 1:
-				fellowAddLog("Pentium Pro");
-				off = 11;
-				break;
-			case 3:
-				fellowAddLog("Pentium II");
-				off = 10;
-				break;
-			case 5:
-				if (l2_cache == 0) {
-					fellowAddLog("Celeron");
-					off = 7;
-				} else {
-					fellowAddLog("Pentium II");
-					off = 10;
-				}
-				break;
-			case 6:
-				if (l2_cache == 128) {
-					fellowAddLog("Celeron");
-					off = 7;
-				} else {
-					fellowAddLog("Pentium II");
-					off = 10;
-				}
-				break;
-			case 7:
-			case 8:
-				fellowAddLog("Pentium III");
-				off = 11;
-				break;
-			}
-		}
-		break;
-
-	/* Cyrix Processors with CPUID */
-	case 'C':
-		switch(cpu_id.model) {
-		case 0:
-			fellowAddLog("Cyrix 6x86MX/MII");
-			off = 16;
-			break;
-		case 4:
-			fellowAddLog("Cyrix GXm");
-			off = 9;
-			break;
-		}
-		return;
-		break;
-
-	/* Unknown processor */
-	default:
-		off = 3;
-		/* Make a guess at the family */
-		switch(cpu_id.type) {
-		case 5:
-			fellowAddLog("586");
-			return;
-		case 6:
-			fellowAddLog("686");
-			return;
-		}
+      /* Get the cache info */
+      for (i = 0; i < 16; i++)
+	{
+	  switch (cpu_id.cache_info[i])
+	    {
+	    case 0x6:
+	      l1_cache += 8;
+	      break;
+	    case 0x8:
+	      l1_cache += 16;
+	      break;
+	    case 0xa:
+	      l1_cache += 8;
+	      break;
+	    case 0xc:
+	      l1_cache += 16;
+	      break;
+	    case 0x40:
+	      l2_cache = 0;
+	      break;
+	    case 0x41:
+	      l2_cache = 128;
+	      break;
+	    case 0x42:
+	    case 0x82:
+	      l2_cache = 256;
+	      break;
+	    case 0x43:
+	      l2_cache = 512;
+	      break;
+	    case 0x44:
+	    case 0x84:
+	      l2_cache = 1024;
+	      break;
+	    case 0x45:
+	    case 0x85:
+	      l2_cache = 2048;
+	      break;
+	    }
 	}
-	fellowAddLog("; L1 Cache: %d KB; L2 Cache: %d KB", l1_cache, l2_cache);
-	fellowAddLog("\n");
 
-	 __try
-     {
-	   speed = cpu_speed();
-	   fellowAddLog("Calculated CPU speed: %f MHz\n", speed);
-     }
-     __except(EXCEPTION_EXECUTE_HANDLER)
-     {
-       fellowAddLog("Can't calculate CPU speed: processor doesn't support the RDTSC instruction.\n");
-     }
+      switch (cpu_id.type)
+	{
+	case 5:
+	  switch (cpu_id.model)
+	    {
+	    case 0:
+	    case 1:
+	    case 2:
+	    case 3:
+	    case 7:
+	      fellowAddLog ("Pentium");
+	      if (l1_cache == 0)
+		{
+		  l1_cache = 8;
+		}
+	      off = 7;
+	      break;
+	    case 4:
+	    case 8:
+	      fellowAddLog ("Pentium-MMX");
+	      if (l1_cache == 0)
+		{
+		  l1_cache = 16;
+		}
+	      off = 11;
+	      break;
+	    }
+	  break;
+	case 6:
+	  switch (cpu_id.model)
+	    {
+	    case 0:
+	    case 1:
+	      fellowAddLog ("Pentium Pro");
+	      off = 11;
+	      break;
+	    case 3:
+	      fellowAddLog ("Pentium II");
+	      off = 10;
+	      break;
+	    case 5:
+	      if (l2_cache == 0)
+		{
+		  fellowAddLog ("Celeron");
+		  off = 7;
+		}
+	      else
+		{
+		  fellowAddLog ("Pentium II");
+		  off = 10;
+		}
+	      break;
+	    case 6:
+	      if (l2_cache == 128)
+		{
+		  fellowAddLog ("Celeron");
+		  off = 7;
+		}
+	      else
+		{
+		  fellowAddLog ("Pentium II");
+		  off = 10;
+		}
+	      break;
+	    case 7:
+	    case 8:
+	      fellowAddLog ("Pentium III");
+	      off = 11;
+	      break;
+	    }
+	}
+      break;
+
+      /* Cyrix Processors with CPUID */
+    case 'C':
+      switch (cpu_id.model)
+	{
+	case 0:
+	  fellowAddLog ("Cyrix 6x86MX/MII");
+	  off = 16;
+	  break;
+	case 4:
+	  fellowAddLog ("Cyrix GXm");
+	  off = 9;
+	  break;
+	}
+      return;
+      break;
+
+      /* Unknown processor */
+    default:
+      off = 3;
+      /* Make a guess at the family */
+      switch (cpu_id.type)
+	{
+	case 5:
+	  fellowAddLog ("586");
+	  return;
+	case 6:
+	  fellowAddLog ("686");
+	  return;
+	}
+    }
+  fellowAddLog ("; L1 Cache: %d KB; L2 Cache: %d KB", l1_cache, l2_cache);
+  fellowAddLog ("\n");
+
+  __try
+  {
+    speed = cpu_speed ();
+    fellowAddLog ("Calculated CPU speed: %f MHz\n", speed);
+  }
+  __except (EXCEPTION_EXECUTE_HANDLER)
+  {
+    fellowAddLog
+      ("Can't calculate CPU speed: processor doesn't support the RDTSC instruction.\n");
+  }
 }
 
 /*===============*/
