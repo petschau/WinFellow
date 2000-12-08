bc82dfe78ad0581b5ec5c2a70befcb4ead1000bf
carfesh <carfesh@5ef2197a-e883-47a9-8f70-2fa63c8abf5e>
2000-12-08 20:24:05 +0000

    cia state added

--- trunk/fellow/SRC/WIN32/MSVC/gui_debugger.h
+++ trunk/fellow/SRC/WIN32/MSVC/gui_debugger.h
@@ -15,6 +15,7 @@
 #define IDC_DEBUG_CPU_BREAK             1021
 #define IDC_DEBUG_CPU_RUN               1022
 #define IDD_DEBUG_MEMORY                1125
+#define IDD_DEBUG_CIA                   1126
 #define ID_DEBUGGER_START               40003
 
 // Next default values for new objects
