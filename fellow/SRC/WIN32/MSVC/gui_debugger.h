9d852fc33bc428a0f41b5227fb61096d7430210e
worfje <worfje@5ef2197a-e883-47a9-8f70-2fa63c8abf5e>
2000-12-08 00:43:41 +0000

    - added second resource file for debugger GUI

---
+++ fellow/SRC/WIN32/MSVC/gui_debugger.h
@@ -0,0 +1,20 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+// Used by GUI_debugger.rc
+//
+#define IDD_DEBUG_CPU                   123
+#define IDC_DEBUG_CPU_STEP              1019
+#define IDC_DEBUG_CPU_STEP_OVER         1020
+#define IDC_DEBUG_CPU_BREAK             1021
+#define IDC_DEBUG_CPU_RUN               1022
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        102
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
