8dc06356e45ce882d39c30a30ad4fa7dd72f119d
carfesh <carfesh@gmx.net>
2000-12-09 17:50:46 +0000

    debugger nearly completed

--- fellow/SRC/WIN32/MSVC/gui_debugger.h
+++ fellow/SRC/WIN32/MSVC/gui_debugger.h
@@ -7,6 +7,7 @@
 #define IDC_DEBUG_MEMORY_UP             1000
 #define IDC_DEBUG_MEMORY_DOWN           1001
 #define IDC_DEBUG_MEMORY_PGUP           1002
+#define IDC_DEBUG_MODRIP                1002
 #define IDC_DEBUG_MEMORY_PGDN           1003
 #define IDC_DEBUG_MEMORY_ASCII          1004
 #define IDC_DEBUG_MEMORY_HEX            1005
@@ -17,6 +18,11 @@
 #define IDD_DEBUG_MEMORY                1125
 #define IDD_DEBUG_CIA                   1126
 #define IDD_DEBUG_FLOPPY                1127
+#define IDD_DEBUG_COPPER                1128
+#define IDD_DEBUG_SPRITES               1129
+#define IDD_DEBUG_SCREEN                1130
+#define IDD_DEBUG_EVENTS                1131
+#define IDD_DEBUG_SOUND                 1132
 #define ID_DEBUGGER_START               40003
 
 // Next default values for new objects
@@ -25,7 +31,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        107
 #define _APS_NEXT_COMMAND_VALUE         40004
-#define _APS_NEXT_CONTROL_VALUE         1002
+#define _APS_NEXT_CONTROL_VALUE         1003
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
