36af2ab2f93e015c3fdaa52cb0ae098daa76a214
peschau <peschau@5ef2197a-e883-47a9-8f70-2fa63c8abf5e>
2001-08-16 18:40:15 +0000

    Redirected relative path func to fswrap.c

--- trunk/fellow/SRC/C/FSNAVIG.C
+++ trunk/fellow/SRC/C/FSNAVIG.C
@@ -13,7 +13,6 @@
 #include "portable.h"
 #include "fswrap.h"
 
-
 /*===========================================================================*/
 /* Module variables                                                          */
 /*===========================================================================*/
@@ -65,93 +64,12 @@ void fsNavigInitializeInstallDir(STR *cmd) {
 }
 
 
-/*==========================================================================*/
-/* Access to files happens relative to the fellow homedirectory if the file */
-/* is on the same drive                                                     */
-/*==========================================================================*/
-
-void fsNavigDecomposePath(STR *decomposed[], STR *str) {
-  LON i, until, j = 0;
-  BOOLE ended = FALSE;
-
-  if (fsNavigHasDrives())
-    i = until = 3;
-  else
-    i = until = 1;
-  if (str[i] == '\0')
-    decomposed[0] = NULL;
-  else {
-    while (!ended) {
-      decomposed[j] = &str[i];
-      while (str[until] != '\0' && str[until] != FS_WRAP_PATH_SEPARATOR_CHAR)
-	until++;
-      if (str[until] == '\0')
-	ended = TRUE;
-      else
-	str[until] = '\0';
-      until++;
-      j++;
-      i = until;
-    }
-    decomposed[j] = NULL;
-  }
-}
-  
-/*===================================================================*/
-/* How this works:                                                   */
-/* As long as hpath and npath is the same, call recursively with one */
-/* element less in the path arrays                                   */
-/* then build relative path from remaining elements                  */
-/*===================================================================*/
-
-void fsNavigBuildRelativePath(STR *hpath[], STR *npath[], STR *buf) {
-  if (hpath[0] == NULL) { /* Bottom of hpath, name is below home */
-    LON i = 0;
-    while (npath[i] != NULL) {
-      strcat(buf, npath[i]);
-      i++;
-      if (npath[i] != NULL)
-	strcat(buf, FS_WRAP_PATH_SEPARATOR_STR);
-    }
-  }
-  else if (stricmp(hpath[0], npath[0]) == 0) /* path element same, take away */
-    fsNavigBuildRelativePath(&hpath[1], &npath[1], buf);
-  else {
-    /* Path no longer the same, add .. for each hpath left, and add */
-    /* remaining part of npath */
-    LON i = 0;
-
-    while (hpath[i++] != NULL)
-      strcat(buf, ".." FS_WRAP_PATH_SEPARATOR_STR);
-    i = 0;
-    while (npath[i] != NULL) {
-      strcat(buf, npath[i]);
-      i++;
-      if (npath[i] != NULL)
-	strcat(buf, FS_WRAP_PATH_SEPARATOR_STR);
-    }
-  }
-}
-
-
 /*===========================================================================*/
 /* Turns the name path into a relative path                                  */
 /*===========================================================================*/
 
 void fsNavigMakeRelativePath(STR *name) {
-  static STR tmpname[FS_WRAP_MAX_PATH_LENGTH];
-  static STR tmphome[FS_WRAP_MAX_PATH_LENGTH];
-  static STR *homedecomposed[128];
-  static STR *namedecomposed[128];
-  
-  /* Expand to full path */
-
-  fsWrapFullPath(name, tmpname);
-  strcpy(tmphome, fs_navig_install_dir->name);
-  fsNavigDecomposePath(homedecomposed, tmphome);
-  fsNavigDecomposePath(namedecomposed, tmpname);
-  name[0] = '\0';
-  fsNavigBuildRelativePath(homedecomposed, namedecomposed, name);
+  fsWrapMakeRelativePath(fs_navig_install_dir->name, name);
 }
 
 
