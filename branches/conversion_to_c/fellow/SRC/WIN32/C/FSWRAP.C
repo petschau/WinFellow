a7597af7eb7ef130d91099b15f48aa1e252d47a2
carfesh <carfesh@5ef2197a-e883-47a9-8f70-2fa63c8abf5e>
2004-05-27 09:57:30 +0000

    updated the header information to include cvs versioning

--- branches/conversion_to_c/fellow/SRC/WIN32/C/FSWRAP.C
+++ branches/conversion_to_c/fellow/SRC/WIN32/C/FSWRAP.C
@@ -1,10 +1,28 @@
-/*===========================================================================*/
-/* Fellow Amiga Emulator                                                     */
-/* Filesystem browsing wrapper for the GUI                                   */
-/* Author: Petter Schau (peschau@online.no)                                  */
-/*                                                                           */
-/* This file is under the GNU Public License (GPL)                           */
-/*===========================================================================*/
+/*=========================================================================*/
+/* Fellow Amiga Emulator                                                   */
+/*                                                                         */
+/* @(#) $Id: FSWRAP.C,v 1.2.2.3 2004-05-27 09:57:30 carfesh Exp $      */
+/*                                                                         */
+/* Filesystem browsing wrapper for the GUI                                 */
+/*                                                                         */
+/* Author: Petter Schau (peschau@online.no)                                */
+/*                                                                         */
+/* Copyright (C) 1991, 1992, 1996 Free Software Foundation, Inc.           */
+/*                                                                         */
+/* This program is free software; you can redistribute it and/or modify    */
+/* it under the terms of the GNU General Public License as published by    */
+/* the Free Software Foundation; either version 2, or (at your option)     */
+/* any later version.                                                      */
+/*                                                                         */
+/* This program is distributed in the hope that it will be useful,         */
+/* but WITHOUT ANY WARRANTY; without even the implied warranty of          */
+/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           */
+/* GNU General Public License for more details.                            */
+/*                                                                         */
+/* You should have received a copy of the GNU General Public License       */
+/* along with this program; if not, write to the Free Software Foundation, */
+/* Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.          */
+/*=========================================================================*/
 
 #include <sys/types.h>
 #include <sys/stat.h>
