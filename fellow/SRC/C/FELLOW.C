74b0ca12d4421e805eb03f05788976c561526428
carfesh <carfesh@gmx.net>
2004-06-08 14:24:58 +0000

    minor updates of headers

--- fellow/SRC/C/FELLOW.C
+++ fellow/SRC/C/FELLOW.C
@@ -1,9 +1,35 @@
+/* @(#) $Id: FELLOW.C,v 1.15.2.3 2004-06-08 14:24:58 carfesh Exp $ */
+/*=========================================================================*/
+/* Fellow Amiga Emulator                                                   */
+/*                                                                         */
+/* This file has overall control over starting and stopping Fellow         */
+/* Everything starts here                                                  */
+/*                                                                         */
+/* Author: Petter Schau (peschau@online.no)                                */
+/*                                                                         */
+/* Copyright (C) 1991, 1992, 1996 Free Software Foundation, Inc.           */
+/*                                                                         */
+/* This program is free software; you can redistribute it and/or modify    */
+/* it under the terms of the GNU General Public License as published by    */
+/* the Free Software Foundation; either version 2, or (at your option)     */
+/* any later version.                                                      */
+/*                                                                         */
+/* This program is distributed in the hope that it will be useful,         */
+/* but WITHOUT ANY WARRANTY; without even the implied warranty of          */
+/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           */
+/* GNU General Public License for more details.                            */
+/*                                                                         */
+/* You should have received a copy of the GNU General Public License       */
+/* along with this program; if not, write to the Free Software Foundation, */
+/* Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.          */
+/*=========================================================================*/
+
+
+
+
 /*============================================================================*/
 /* Fellow Amiga Emulator                                                      */
-/* This file has overall control over starting and stopping Fellow            */
-/* Everything starts here                                                     */
-/*                                                                            */
-/* Author: Petter Schau (peschau@online.no)                                   */
+/*                                  */
 /*                                                                            */
 /* This file is under the GNU Public License (GPL)                            */
 /*============================================================================*/
