2e706ecf2fa83b97e0d144808a4a821e0c9cadbb
carfesh <carfesh@gmx.net>
2004-06-08 14:28:57 +0000

    minor updates of headers

--- fellow/SRC/C/BUS.C
+++ fellow/SRC/C/BUS.C
@@ -1,11 +1,27 @@
-/*============================================================================*/
+/* @(#) $Id: BUS.C,v 1.2 2004-06-08 14:28:57 carfesh Exp $ */
+/*=========================================================================*/
 /* Fellow Amiga Emulator                                                      */
+/*                                                                         */
 /* Bus Event Scheduler Initialization                                         */
 /*                                                                            */
 /* Author: Petter Schau (peschau@online.no)                                   */
 /*                                                                            */
-/* This file is under the GNU Public License (GPL)                            */
-/*============================================================================*/
+/* Copyright (C) 1991, 1992, 1996 Free Software Foundation, Inc.           */
+/*                                                                         */
+/* This program is free software; you can redistribute it and/or modify    */
+/* it under the terms of the GNU General Public License as published by    */
+/* the Free Software Foundation; either version 2, or (at your option)     */
+/* any later version.                                                      */
+/*                                                                         */
+/* This program is distributed in the hope that it will be useful,         */
+/* but WITHOUT ANY WARRANTY; without even the implied warranty of          */
+/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           */
+/* GNU General Public License for more details.                            */
+/*                                                                         */
+/* You should have received a copy of the GNU General Public License       */
+/* along with this program; if not, write to the Free Software Foundation, */
+/* Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.          */
+/*=========================================================================*/
 
 #include "portable.h"
 #include "renaming.h"
@@ -17,6 +33,10 @@
 #include "fmem.h"
 #include "blit.h"
 #include "bus.h"
+#include "graph.h"
+#include "floppy.h"
+#include "kbd.h"
+#include "sound.h"
 
 
 ULO debugging;
--- fellow/SRC/C/FELLOW.C
+++ fellow/SRC/C/FELLOW.C
@@ -1,12 +1,28 @@
-/*============================================================================*/
+/* @(#) $Id: FELLOW.C,v 1.18 2004-06-08 14:28:57 carfesh Exp $ */
+/*=========================================================================*/
 /* Fellow Amiga Emulator                                                      */
+/*                                                                         */
 /* This file has overall control over starting and stopping Fellow            */
 /* Everything starts here                                                     */
 /*                                                                            */
 /* Author: Petter Schau (peschau@online.no)                                   */
 /*                                                                            */
-/* This file is under the GNU Public License (GPL)                            */
-/*============================================================================*/
+/* Copyright (C) 1991, 1992, 1996 Free Software Foundation, Inc.           */
+/*                                                                         */
+/* This program is free software; you can redistribute it and/or modify    */
+/* it under the terms of the GNU General Public License as published by    */
+/* the Free Software Foundation; either version 2, or (at your option)     */
+/* any later version.                                                      */
+/*                                                                         */
+/* This program is distributed in the hope that it will be useful,         */
+/* but WITHOUT ANY WARRANTY; without even the implied warranty of          */
+/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           */
+/* GNU General Public License for more details.                            */
+/*                                                                         */
+/* You should have received a copy of the GNU General Public License       */
+/* along with this program; if not, write to the Free Software Foundation, */
+/* Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.          */
+/*=========================================================================*/
 
 #include <time.h>
 #include "portable.h"
