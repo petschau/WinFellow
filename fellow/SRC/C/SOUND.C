7efd13f24526babcdef9457512386d10d5363c3a
carfesh <carfesh@gmx.net>
2004-06-08 14:28:57 +0000

    minor updates of headers

--- fellow/SRC/C/SOUND.C
+++ fellow/SRC/C/SOUND.C
@@ -1,12 +1,27 @@
-/*============================================================================*/
-/* Fellow Amiga Emulator                                                      */
-/* Sound emulation                                                            */
-/*                                                                            */
-/* Author: Petter Schau (peschau@online.no)                                   */
-/*                                                                            */
-/* This file is under the GNU Public License (GPL)                            */
-/*============================================================================*/
-
+/* @(#) $Id: SOUND.C,v 1.4.2.1 2004-06-08 14:24:58 carfesh Exp $ */
+/*=========================================================================*/
+/* Fellow Amiga Emulator                                                   */
+/*                                                                         */
+/* Sound emulation                                                         */
+/*                                                                         */
+/* Author: Petter Schau (peschau@online.no)                                */
+/*                                                                         */
+/* Copyright (C) 1991, 1992, 1996 Free Software Foundation, Inc.           */
+/*                                                                         */
+/* This program is free software; you can redistribute it and/or modify    */
+/* it under the terms of the GNU General Public License as published by    */
+/* the Free Software Foundation; either version 2, or (at your option)     */
+/* any later version.                                                      */
+/*                                                                         */
+/* This program is distributed in the hope that it will be useful,         */
+/* but WITHOUT ANY WARRANTY; without even the implied warranty of          */
+/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           */
+/* GNU General Public License for more details.                            */
+/*                                                                         */
+/* You should have received a copy of the GNU General Public License       */
+/* along with this program; if not, write to the Free Software Foundation, */
+/* Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.          */
+/*=========================================================================*/
 
 #include "portable.h"
 #include "renaming.h"
