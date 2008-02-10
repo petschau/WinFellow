d3afd88dc5f7b0f08adc3da069043936ecaf48fc
carfesh <carfesh@5ef2197a-e883-47a9-8f70-2fa63c8abf5e>
2008-02-10 09:26:43 +0000

    Windows filesystem operations

--- trunk/fellow/SRC/WIN32/INCLUDE/fileops.h
+++ trunk/fellow/SRC/WIN32/INCLUDE/fileops.h
@@ -1,6 +1,32 @@
+/* @(#) $Id: fileops.h,v 1.2 2008-02-10 09:26:43 carfesh Exp $             */
+/*=========================================================================*/
+/* Fellow Amiga Emulator                                                   */
+/*                                                                         */
+/* Filesystem operations                                                   */
+/*                                                                         */
+/* Author: Torsten Enderling (carfesh@gmx.net)                             */
+/*                                                                         */
+/* Copyright (C) 1991, 1992, 1996 Free Software Foundation, Inc.           */
+/*                                                                         */
+/* This program is free software; you can redistribute it and/or modify    */
+/* it under the terms of the GNU General Public License as published by    */
+/* the Free Software Foundation; either version 2, or (at your option)     */
+/* any later version.                                                      */
+/*                                                                         */
+/* This program is distributed in the hope that it will be useful,         */
+/* but WITHOUT ANY WARRANTY; without even the implied warranty of          */
+/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           */
+/* GNU General Public License for more details.                            */
+/*                                                                         */
+/* You should have received a copy of the GNU General Public License       */
+/* along with this program; if not, write to the Free Software Foundation, */
+/* Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.          */
+/*=========================================================================*/
+
 #ifndef FILEOPS_H
 #define FILEOPS_H
 
 BOOLE fileopsGetFellowLogfileName(char *);
+BOOLE fileopsGetGenericFileName(char *, const char*);
 
 #endif // FILEOPS_H
