f49cfd28bcbad18db99cc8e283af1570813a6303
peschau <peschau@5ef2197a-e883-47a9-8f70-2fa63c8abf5e>
2008-02-20 00:11:25 +0000

    Tweaked blitter timing. (Global trash). Added GPL header and version string.

--- trunk/fellow/SRC/C/BLIT.C
+++ trunk/fellow/SRC/C/BLIT.C
@@ -1,12 +1,27 @@
-/*============================================================================*/
-/* Fellow Amiga Emulator                                                      */
-/* Blitter Initialization                                                     */
-/*                                                                            */
-/* Authors: Petter Schau                                                      */
-/*          Worfje                                                            */
-/*                                                                            */
-/* This file is under the GNU Public License (GPL)                            */
-/*============================================================================*/
+/* @(#) $Id: BLIT.C,v 1.12 2008-02-20 00:11:25 peschau Exp $ */
+/*=========================================================================*/
+/* Fellow                                                                  */
+/* Blitter Implementation                                                  */
+/*                                                                         */
+/* Authors: Petter Schau                                                   */
+/*          Worfje                                                         */
+/*                                                                         */
+/* Copyright (C) 1991, 1992, 1996 Free Software Foundation, Inc.           */
+/*                                                                         */
+/* This program is free software; you can redistribute it and/or modify    */
+/* it under the terms of the GNU General Public License as published by    */
+/* the Free Software Foundation; either version 2, or (at your option)     */
+/* any later version.                                                      */
+/*                                                                         */
+/* This program is distributed in the hope that it will be useful,         */
+/* but WITHOUT ANY WARRANTY; without even the implied warranty of          */
+/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           */
+/* GNU General Public License for more details.                            */
+/*                                                                         */
+/* You should have received a copy of the GNU General Public License       */
+/* along with this program; if not, write to the Free Software Foundation, */
+/* Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.          */
+/*=========================================================================*/
 
 #include "defs.h"
 #include "fellow.h"
@@ -807,12 +822,16 @@ void blitInitiate(void)
 
   if (cycle_free == 0)
   {
-    if ((dmaconr & 0x400)) cpu_chip_cycles = cycle_length;
-    else cycle_length += (cycle_length/5);
+    if (!(dmaconr & 0x400))
+    {
+      cycle_length += (cycle_length/5);
+      cycle_free = (cycle_length/5);
+    }
   }
   blit_cycle_length = cycle_length;
   blit_cycle_free = cycle_free;
   blit_started = TRUE;
+  cpu_chip_cycles = blit_cycle_length - blit_cycle_free;
   dmaconr |= 0x4000; /* Blitter busy bit */
   blitterInsertEvent(cycle_length + bus_cycle);
 }
