42d689d6ae6c08c347cafdf7fbe0ab3ac4af3598
Petter Schau <petschau@users.noreply.github.com>
2008-02-20 23:54:49 +0000

    ADDX,ABCD fix.

--- fellow/SRC/68kgenerate/68kgenerate.c
+++ fellow/SRC/68kgenerate/68kgenerate.c
@@ -1,3 +1,28 @@
+/* @(#) $Id: 68kgenerate.c,v 1.3 2008-02-20 23:54:49 peschau Exp $          */
+/*=========================================================================*/
+/* Fellow                                                                  */
+/*                                                                         */
+/* Code generator for the 68k emulation.                                   */
+/*                                                                         */
+/* Author: Petter Schau                                                    */
+/*                                                                         */
+/* Copyright (C) 1991, 1992, 1996 Free Software Foundation, Inc.           */
+/*                                                                         */
+/* This program is free software; you can redistribute it and/or modify    */
+/* it under the terms of the GNU General Public License as published by    */
+/* the Free Software Foundation; either version 2, or (at your option)     */
+/* any later version.                                                      */
+/*                                                                         */
+/* This program is distributed in the hope that it will be useful,         */
+/* but WITHOUT ANY WARRANTY; without even the implied warranty of          */
+/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           */
+/* GNU General Public License for more details.                            */
+/*                                                                         */
+/* You should have received a copy of the GNU General Public License       */
+/* along with this program; if not, write to the Free Software Foundation, */
+/* Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.          */
+/*=========================================================================*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -1123,7 +1148,7 @@ unsigned int cgAddx(cpu_data *cpudata, cpu_instruction_info i)
   unsigned int regx_cpu_data_index = 0;
   unsigned int regy_cpu_data_index = 1;
   char regtype = i.reg[0];
-  unsigned int eano = (regtype == 'D') ? 0 : (((stricmp(i.instruction_name, "ADDX") == 0) || (stricmp(i.instruction_name, "ABCD") == 0)) ? 3 : 4);
+  unsigned int eano = (regtype == 'D') ? 0 : 4;
   unsigned int regx;
   unsigned int regy;
   unsigned int icount = 0;
