477ccc2e890fff489568b2afbde429ddedce122a
carfesh <carfesh@gmx.net>
2013-01-04 13:03:58 +0000

    - updated to include markdown files from documentation directory

--- fellow/doxygen/Build-Doxygen-Documentation.cmd
+++ fellow/doxygen/Build-Doxygen-Documentation.cmd
@@ -12,10 +12,11 @@ pdflatex refman.tex
 
 cd ..
 
-taskkill AcroRd32.exe
+taskkill /IM AcroRd32.exe
 del  WinFellow-doxygen.pdf.bak
 ren  WinFellow-doxygen.pdf WinFellow-doxygen.pdf.bak
 copy latex\refman.pdf .\WinFellow-doxygen.pdf
 
 del latex /S /Q
 rmdir latex
+del *.tmp /Q
