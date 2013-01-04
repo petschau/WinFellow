85af397e7ed999e00befe3afd0064019613b17b5
carfesh <carfesh@gmx.net>
2013-01-04 11:06:19 +0000

    - initial doxygen configuration for WinFellow

---
+++ fellow/doxygen/Build-Doxygen-Documentation.cmd
@@ -0,0 +1,21 @@
+@echo off
+del latex /S /Q
+rmdir latex
+
+doxygen Doxyfile > WinFellow-doxygen.log
+
+cd latex
+
+pdflatex refman.tex
+makeindex refman.idx
+pdflatex refman.tex
+
+cd ..
+
+taskkill AcroRd32.exe
+del  WinFellow-doxygen.pdf.bak
+ren  WinFellow-doxygen.pdf WinFellow-doxygen.pdf.bak
+copy latex\refman.pdf .\WinFellow-doxygen.pdf
+
+del latex /S /Q
+rmdir latex
