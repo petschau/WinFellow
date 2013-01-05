ab1183eb3009ed6369b60734da605fdd81fc14f6
carfesh <carfesh@5ef2197a-e883-47a9-8f70-2fa63c8abf5e>
2013-01-05 23:20:44 +0000

    - fix for systems without Amiga Forever installed

--- trunk/fellow/SRC/WIN32/C/fileops.c
+++ trunk/fellow/SRC/WIN32/C/fileops.c
@@ -1,4 +1,4 @@
-/* @(#) $Id: fileops.c,v 1.5 2012-12-07 14:05:43 carfesh Exp $             */
+/* @(#) $Id: fileops.c,v 1.6 2013-01-05 23:20:44 carfesh Exp $             */
 /*=========================================================================*/
 /* Fellow                                                                  */
 /*                                                                         */
@@ -29,58 +29,66 @@
 
 #include "defs.h"
 
-/* fileopsResolveVariables                                          */
-/* resolve environment variables in file/folder names               */
+/** @file
+ * The fileops module contains abtract functions to generate filenames in a
+ * platform specific manner.
+ */
 
-BOOLE fileopsResolveVariables(const char *szPath, char *szNewPath)
-{
+/** resolve environment variables in file/folder names.
+ * @param[in] szPath path name to resolve
+ * @param[out] szNewPath path name with resolved variables
+ * @return TRUE, if variable was successfully resolved, FALSE otherwise.
+ */
+BOOLE fileopsResolveVariables(const char *szPath, char *szNewPath) {
   DWORD nRet;
 
   nRet = ExpandEnvironmentStrings(szPath, szNewPath, CFG_FILENAME_LENGTH);
 
   if(nRet < CFG_FILENAME_LENGTH)
-    if( nRet )
-    {
-      szPath = szNewPath;
-      return TRUE;
+    if( nRet ) {
+      if(strcmp(szPath, szNewPath) == 0)
+        return FALSE;
+      else
+        return TRUE;
     }
   szNewPath = NULL;
   return FALSE;
 }
 
-/* fileopsGetGenericFileName                                        */
-/* build generic filename pointing to Application Data\WinFellow    */
-/* AmigaForever Amiga files path will be preferred over AppData     */
-/* return TRUE if successfull, FALSE otherwise                      */
-
+/** build generic filename pointing to "Application Data\Roaming\WinFellow";
+ * AmigaForever Amiga files path will be preferred over AppData when 
+ * compiling a RetroPlatform specific build.
+ * @return TRUE if successful, FALSE otherwise
+ */
 BOOLE fileopsGetGenericFileName(char *szPath, const char *szSubDir, const char *filename)
 {
   HRESULT hr;
   
-  // first check if AmigaForever is installed, use Amiga files path first
+#ifdef RETRO_PLATFORM
+  // first check if AmigaForever is installed, prefer Amiga files path
   if(!fileopsResolveVariables("%AMIGAFOREVERDATA%", szPath)) {
-    if (SUCCEEDED (hr = SHGetFolderPathAndSubDir(NULL,                              // hWnd	
-                                                 CSIDL_APPDATA | CSIDL_FLAG_CREATE, // csidl
-                                                 NULL,                              // hToken
-                                                 SHGFP_TYPE_CURRENT,                // dwFlags
-                                                 szSubDir,                          // pszSubDir
-                                                 szPath)))                          // pszPath
-    {
+#endif
+    if (SUCCEEDED (hr = SHGetFolderPathAndSubDir(NULL,                              	
+                                                 CSIDL_APPDATA | CSIDL_FLAG_CREATE,
+                                                 NULL,
+                                                 SHGFP_TYPE_CURRENT,
+                                                 szSubDir,
+                                                 szPath))) {
 	    PathAppend(szPath, TEXT(filename));
       return TRUE;
     }
-    else 
-    {
+    else {
       strcpy(szPath, filename);
       return FALSE;
     }
+#ifdef RETRO_PLATFORM
   }
-  else 
-  {
+  else {
     PathAppend(szPath, szSubDir);
     PathAppend(szPath, TEXT(filename));
     return TRUE;
   }
+#endif
 }
 
 /* fileopsGetFellowLogfileName                                      */
