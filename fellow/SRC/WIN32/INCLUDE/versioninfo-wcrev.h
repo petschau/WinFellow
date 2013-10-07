cbcbb1638b17d3ecd32358c0609ea4860c76a7a5
carfesh <carfesh@gmx.net>
2013-10-07 11:51:13 +0000

    version number increased to 0.5.2 as preparation for new release documentation update

--- fellow/SRC/WIN32/INCLUDE/versioninfo-wcrev.h
+++ fellow/SRC/WIN32/INCLUDE/versioninfo-wcrev.h
@@ -1,8 +1,8 @@
 #ifndef VERSIONINFO_H
 #define VERSIONINFO_H
 
-#define FELLOWVERSION        "WinFellow alpha v0.5.1 r$WCREV$ (SVN)"
-#define FELLOWLONGVERSION    "WinFellow Amiga Emulator alpha v0.5.1.$WCREV$ - SVN" 
-#define FELLOWNUMERICVERSION "0.5.1.$WCREV$"
+#define FELLOWVERSION        "WinFellow alpha v0.5.2 r$WCREV$ (SVN)"
+#define FELLOWLONGVERSION    "WinFellow Amiga Emulator alpha v0.5.2.$WCREV$ - SVN" 
+#define FELLOWNUMERICVERSION "0.5.2.$WCREV$"
 
 #endif
