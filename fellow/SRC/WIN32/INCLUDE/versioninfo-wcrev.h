390a084eacedfe3b8b09b03261432d44e5b73dd7
carfesh <carfesh@gmx.net>
2016-01-08 10:36:32 +0100

    fix versioninfo generation/compilation on branches

--- fellow/SRC/WIN32/INCLUDE/versioninfo-wcrev.h
+++ fellow/SRC/WIN32/INCLUDE/versioninfo-wcrev.h
@@ -1,15 +1,8 @@
 #ifndef VERSIONINFO_H
 #define VERSIONINFO_H
 
-#define master 1
-#if $WCBRANCH$ == 1
-    #define FELLOWVERSION        "WinFellow v0.5.4 r$WCREV$ (Git-$WCCOMMITSHORT$)"
-    #define FELLOWLONGVERSION    "WinFellow Amiga Emulator v0.5.4.$WCREV$ (Git-$WCCOMMITSHORT$)" 
-#else
-    #define FELLOWVERSION        "WinFellow v0.5.4 r$WCREV$ (Git-$WCBRANCH$-$WCCOMMITSHORT$)"
-    #define FELLOWLONGVERSION    "WinFellow Amiga Emulator v0.5.4.$WCREV$ (Git-$WCBRANCH$-$WCCOMMITSHORT$)" 
-#endif
-
+#define FELLOWVERSION        "WinFellow v0.5.4 r$WCREV$ (Git-$WCBRANCH$-$WCCOMMITSHORT$)"
+#define FELLOWLONGVERSION    "WinFellow Amiga Emulator v0.5.4.$WCREV$ (Git-$WCBRANCH$-$WCCOMMITSHORT$)" 
 #define FELLOWNUMERICVERSION "0.5.4.$WCREV$"
 
 #endif
