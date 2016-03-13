c44dcd540ac081ea663454e857ec4c20fd0ab053
Petter Schau <petschau@gmail.com>
2016-03-13 13:40:23 +0100

    Switched to using a point sampler for magnification.

--- fellow/SRC/WIN32/DXGI/GfxDrvDXGI.cpp
+++ fellow/SRC/WIN32/DXGI/GfxDrvDXGI.cpp
@@ -563,7 +563,8 @@ bool GfxDrvDXGI::CreatePixelShader()
 
   // Create a texture sampler state description.
   D3D11_SAMPLER_DESC samplerDesc;
-  samplerDesc.Filter = D3D11_FILTER_MIN_MAG_MIP_LINEAR;
+//  samplerDesc.Filter = D3D11_FILTER_MIN_MAG_MIP_LINEAR;
+  samplerDesc.Filter = D3D11_FILTER_MIN_LINEAR_MAG_MIP_POINT;  
   samplerDesc.AddressU = D3D11_TEXTURE_ADDRESS_WRAP;
   samplerDesc.AddressV = D3D11_TEXTURE_ADDRESS_WRAP;
   samplerDesc.AddressW = D3D11_TEXTURE_ADDRESS_WRAP;
