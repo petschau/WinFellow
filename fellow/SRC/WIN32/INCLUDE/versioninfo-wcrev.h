4ee2f16cdb8f95b3fa6ff98ad4a14f3525c06214
Torsten Enderling <carfesh@gmx.net>
2017-12-22 11:03:30 +0100

    update version number to 0.5.7
    update documentation

--- fellow/SRC/WIN32/INCLUDE/versioninfo-wcrev.h
+++ fellow/SRC/WIN32/INCLUDE/versioninfo-wcrev.h
@@ -1,8 +1,8 @@
 #ifndef VERSIONINFO_H
 #define VERSIONINFO_H
 
-#define FELLOWVERSION        "WinFellow v0.5.6 r$WCREV$ (Git-$WCBRANCH$-$WCCOMMITSHORT$)"
-#define FELLOWLONGVERSION    "WinFellow Amiga Emulator v0.5.6.$WCREV$ (Git-$WCBRANCH$-$WCCOMMITSHORT$)" 
-#define FELLOWNUMERICVERSION "0.5.6.$WCREV$"
+#define FELLOWVERSION        "WinFellow v0.5.7 r$WCREV$ (Git-$WCBRANCH$-$WCCOMMITSHORT$)"
+#define FELLOWLONGVERSION    "WinFellow Amiga Emulator v0.5.7.$WCREV$ (Git-$WCBRANCH$-$WCCOMMITSHORT$)" 
+#define FELLOWNUMERICVERSION "0.5.7.$WCREV$"
 
 #endif
