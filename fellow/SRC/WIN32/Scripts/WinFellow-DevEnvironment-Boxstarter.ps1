3c23da7952222d348eb274d107d00713ae7038e5
Torsten Enderling <torsten.enderling@gmail.com>
2021-02-24 04:13:05 +0100

    remove reboot requirement from Boxstarter script

--- fellow/SRC/WIN32/Scripts/WinFellow-DevEnvironment-Boxstarter.ps1
+++ fellow/SRC/WIN32/Scripts/WinFellow-DevEnvironment-Boxstarter.ps1
@@ -5,12 +5,10 @@
 
 # Created 29.01.2021 by Torsten Enderling
 
-$Credential = Get-Credential $env:username
-
 If(-Not(Test-Path $env:programdata\Boxstarter\Boxstarter.Chololatey\Install-BoxStarterPackage.ps1)) {
     . { iwr -useb https://boxstarter.org/bootstrapper.ps1 } | iex; Get-Boxstarter -Force
 }
 
 $PackageName = Resolve-Path -Path ".\WinFellow-DevEnvironment-Boxstarter.txt"
 
-Install-BoxStarterPackage -PackageName $PackageName -Credential $Credential -StopOnPackageFailure
+Install-BoxStarterPackage -PackageName $PackageName -StopOnPackageFailure -DisableReboots
