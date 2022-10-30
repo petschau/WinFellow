8e7634b01f592879003fce8137e4d3ce3b36fd6f
Petter Schau <petschau@gmail.com>
2022-10-30 09:30:00 +0100

    Experiment wip, test CMake build with clang and no windows

---
+++ fellow/SRC/fellow/os/null/versioninfo.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#define FELLOWVERSION "WinFellow v0.5.9 r1219 (Git-refactoring-a6dfe4c)"
+#define FELLOWLONGVERSION "WinFellow Amiga Emulator v0.5.9.1219 (Git-refactoring-a6dfe4c)"
+#define FELLOWNUMERICVERSION "0.5.9.1219"
